d4d570c9abf042ccd95cd018f54fd5de9e111818
Sameer Kadu <Samirkadu8@gmail.com>
2024-04-21 04:07:30 +0530

    merge sort update

--- merg sort recursion/qsr.java
+++ merg sort recursion/qsr.java
@@ -1,68 +1,65 @@
 import java.util.*;
-
-public class qsr {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a[] = new int[5];
-        for(int i=0;i<a.length;i++)
-        {
-            a[i]= sc.nextInt();
-        }
-        sortArray(a, 0, 4);
-        printarray(a);
-    }
-
-    public static void printarray(int a[]) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-        }
-        return;
-    }
-
-public static void sortArray(int a[], int s, int e)
+public class qsr
 {
-    if(s >= e)
+    public static void main(String[] args) 
     {
-        return;
-    }
-    int mid = s + (e-s)/2;
-    sortArray(a, s, mid);
-    sortArray(a, mid+1, e);
-    merg(a, s, e, mid);
-}
-
-    public static void merg(int a[], int s, int e, int m) {
-        int k[] = new int[e - s + 1];
-        int i = s;
-        int j = m + 1;
-        int b = 0;
-        while ((i <= m) && (j <= e)) {
-            if (a[i] < a[j]) {
-                k[b] = a[i];
-                i++;
-            } else {
-                k[b] = a[j];
-                j++;
-            }
-            b++;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
         }
-        while (i <= m) {
-            k[b] = a[i];
-            i++;
-            b++;
+        solution(a);
+        for(int nums:a)
+        {
+            System.out.print(nums+ " ");
         }
-        while (j <= e) {
-            k[b] = a[j];
-            b++;
-            j++;
-        }
-        for (b = 0, i=s; b < k.length; b++,i++) {
-            a[i] = k[b];
-            System.out.print(k[b]+" ");
-            
-        }
-        System.out.println();
         
     }
+    public static void solution(int arr[]) 
+    {
+        if(arr == null || arr.length <=1 )
+        {
+            return ;
+        }
+        sort(arr, 0 , arr.length -1 );
+    }
+    public static void sort(int arr[],int start, int end)
+    {
+        if(start < end)
+        {
+            int mid = start + (end - start)/2;
+            sort(arr, start , mid);
+            sort(arr, mid+1, end);
+            merge(arr, start, mid, end);
+        }
+    } 
+    public static void merge(int arr[],int start, int mid, int end)
+    {
+        int temp[]=new int [end-start+1];
+        int i=0,j=0,k=0;
+        while((i<=mid-start) && (j <= end-(mid+1)))
+        {
+            if(arr[start+i] < arr[mid+1+j])
+            {
+                temp [k++]=arr[start+i++];
+            }
+            else{
+                temp [k++] = arr[mid+1+j++];
+            }
+        }
+        while(i <= mid-start)
+        {
+            temp[k++]=arr[start+i++];
+        }
+        while(j <= end-(mid+1))
+        {
+            temp[k++]=arr[mid+1+j++];
+        }
+        for(int l=0;l<temp.length;l++)
+        {
+            arr[start+l]=temp[l];
+        }
+    }    
 }
